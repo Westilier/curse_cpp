6d3d39a2579a076d0d71816c71db6fc7a9e0a0c1
Westilier <vlad2007Gvv@gmail.com>
2024-05-27 20:03:56 +0300

    поправил расположение проекта

---
+++ lesson_14/homework_2/task1/task1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include<string>
+
+int mystrcmp(const char* str1, const char* str2);
+
+int StringToNumber(std::string str)
+{
+    size_t number = 0;
+    size_t length = str.size() - 1;
+
+    for (size_t i = 0; i < length; ++i)
+    {
+        number = number * 10 + str[i] - '0';
+    }
+
+    return number;
+}
+
+std::string NumberToString(int number)
+{
+    std::string str = std::to_string(number);
+
+    return str;
+}
+
+void Uppercase(std::string str1)
+{
+    size_t length = str1.size() - 1;
+
+    for (size_t i = 0; i < length; ++i)
+        if (str1[i] < 'z' && str1[i] > 'a')
+        {
+            str1[i] -= 32;
+        }
+}
+
+void Lowercase(std::string str1)
+{
+    size_t length = str1.size() - 1;
+
+    for (size_t i = 0; i < length; ++i)
+        if (str1[i] < 'Z' && str1[i] > 'A')
+        {
+            str1[i] += 32;
+        }
+}
+
+void mystrrev(char* str)
+{
+    reverse()
+}
+int main()
+{
+    std::cout << NumberToString(123);
+
+    return 0;
+}
