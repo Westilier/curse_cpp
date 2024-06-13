1b0799a72688943e77294205059a1bcb814ae7ba
Westilier <vlad2007Gvv@gmail.com>
2024-06-13 17:18:24 +0300

    добавлено дз 18.2

---
+++ week_18/homework2/task_1/task_1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+
+struct ComplexNumber {
+
+    double real;
+    double imaginary;
+
+};
+
+ComplexNumber Add(ComplexNumber a, ComplexNumber b)
+{
+    ComplexNumber result;
+    result.real = a.real + b.real;
+    result.imaginary = a.imaginary + b.imaginary;
+    return result;
+}
+
+ComplexNumber subtract(ComplexNumber a, ComplexNumber b)
+{
+    ComplexNumber result;
+    result.real = a.real - b.real;
+    result.imaginary = a.imaginary - b.imaginary;
+    return result;
+}
+
+ComplexNumber multiply(ComplexNumber a, ComplexNumber b)
+{
+    ComplexNumber result;
+    result.real = a.real * b.real - a.imaginary * b.imaginary;
+    result.imaginary = a.real * b.imaginary + a.imaginary * b.real;
+    return result;
+}
+
+ComplexNumber divide(ComplexNumber a, ComplexNumber b)
+{
+    ComplexNumber result;
+    result.real = (a.real * b.real + a.imaginary * b.imaginary) / (b.real * b.real + b.imaginary * b.imaginary);
+    result.imaginary = a.real * b.imaginary - a.imaginary * b.real / (b.real * b.real + b.imaginary * b.imaginary);
+    return result;
+}
+int main()
+{
+    ComplexNumber num1 = { 1,2 };
+    ComplexNumber num2 = { 3,3 };
+
+    std::cout << Add(num1, num2).real <<" " << Add(num1, num2).imaginary <<"i"<< std::endl;
+
+    std::cout << subtract(num1, num2).real << " " << subtract(num1, num2).imaginary << "i" << std::endl;
+
+    std::cout << multiply(num1, num2).real << " " << multiply(num1, num2).imaginary << "i" << std::endl;
+
+    std::cout << divide(num1, num2).real << " " << divide(num1, num2).imaginary << "i" << std::endl;
+
+    return 0;
+}
+
---
+++ week_18/homework2/task_2/task_2.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+struct car {
+    std::string length;
+    std::string clearance;
+    std::string engineCapacity;
+    std::string powerEngine;
+    std::string wheelDiameter;
+    std::string color;
+    std::string gearboxType;
+};
+
+car initialization(car car)
+{
+    std::cout << "Введите длину автомобиля ";
+    getline(std::cin, car.length);
+    std::cout << "Введите клиренс автомобиля ";
+    getline(std::cin, car.clearance);
+    std::cout << "Введите объём двигателя автомобиля ";
+    getline(std::cin, car.engineCapacity);
+    std::cout << "Введите мощность двигателя автомобиля ";
+    getline(std::cin, car.powerEngine);
+    std::cout << "Введите диаметр колес автомобиля ";
+    getline(std::cin, car.wheelDiameter);
+    std::cout << "Введите цвет автомобиля (в И.п) ";
+    getline(std::cin, car.color);
+    std::cout << "Введите тип коробки передач автомобиля (первые 3 буквы) ";
+    getline(std::cin, car.gearboxType);
+}
+
+void OutputCar(car car)
+{
+    std::cout << "Длину автомобиля " << car.length << std::endl;
+    std::cout << "Клиренс автомобиля" << car.clearance << std::endl;
+    std::cout << "Объём двигателя автомобиля " << car.engineCapacity << std::endl;
+    std::cout << "Мощность двигателя автомобиля " << car.powerEngine << std::endl;
+    std::cout << "Диаметр колес автомобиля " << car.wheelDiameter << std::endl;
+    std::cout << "Цвет автомобиля " << car.color << std::endl;
+    std::cout << "Тип коробки передач автомобиля " << car.gearboxType << std::endl;
+}
+
+int SearchByValue(std::vector<car> cars, std::string value)
+{
+    for (size_t i = 0; i < cars.size(); ++i)
+    {
+        if (cars[i].length == value)
+        {
+            return i;
+        }
+        else if (cars[i].clearance == value)
+        {
+            return i;
+        }
+        else if (cars[i].engineCapacity == value)
+        {
+            return i;
+        }
+        else if (cars[i].powerEngine == value)
+        {
+            return i;
+        }
+        else if (cars[i].wheelDiameter == value)
+        {
+            return i;
+        }
+        else if (cars[i].color == value)
+        {
+            return i;
+        }
+        else if (cars[i].gearboxType == value)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+}
