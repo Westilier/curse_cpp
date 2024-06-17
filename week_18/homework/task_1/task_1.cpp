7aba87854625ffa67e61884e2a6f616eabece54d
Westilier <vlad2007Gvv@gmail.com>
2024-06-17 18:55:27 +0300

    исправлена  setlocale  и слипание при в вводе

--- week_18/homework/task_1/task_1.cpp
+++ week_18/homework/task_1/task_1.cpp
@@ -2,15 +2,16 @@
 #include <vector>
 #include <string>
 
+#include "Windows.h"
 
 void SortAscending(std::vector<std::string>& surnames)
 {
     size_t count = surnames.size();
-    for (size_t i = 0; i < count; i++) 
+    for (size_t i = 0; i < count; i++)
     {
-        for (size_t j = 0; j < count-1; j++) 
+        for (size_t j = 0; j < count - 1; j++)
         {
-            if (surnames[j] > surnames[j + 1]) 
+            if (surnames[j] > surnames[j + 1])
             {
                 std::swap(surnames[j], surnames[j + 1]);
             }
@@ -20,16 +21,19 @@ void SortAscending(std::vector<std::string>& surnames)
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
     size_t count = 5;
     std::vector<std::string> surnames;
-    surnames.resize(count);
 
     for (size_t i = 0; i < count; ++i)
     {
-        std::cout << "Введите фамилию студента";
-        getline(std::cin, surnames[i]);
+        std::cout << "Введите фамилию студента ";
+        std::string str;
+        std::cout << str;
+        getline(std::cin,str);
+        surnames.push_back(str);
     }
 
     SortAscending(surnames);
