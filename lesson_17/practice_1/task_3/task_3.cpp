e9db680ff382b92d3306731766b0e1bee41d0eaa
Westilier <vlad2007Gvv@gmail.com>
2024-06-17 20:11:45 +0300

    добавлена практика 17.1

---
+++ lesson_17/practice_1/task_3/task_3.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+
+void AddColInMatrixStart(size_t** arr, size_t& cols, size_t& rows,size_t position)//фунция из задания
+{
+    rows += 1;
+    arr[rows - 1] = new size_t[cols];
+
+    for (size_t i = rows - 1; i > position; --i)
+    {
+        for (size_t j = 0; j < cols; ++j)
+        {
+            arr[i][j] = arr[i - 1][j];
+        }
+    }
+    for (size_t i = 0; i < cols; ++i)
+    {
+        arr[position][i] = 0;
+    }
+}
+
+void OutputMatrix(size_t** arr, size_t& cols, size_t& rows)
+{
+    for (size_t i = 0; i < rows; ++i)
+    {
+        for (size_t j = 0; j < cols; ++j)
+        {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void InitializationMatrix(size_t** arr, size_t& cols, size_t& rows)
+{
+    size_t x = 1;
+    for (size_t i = 0; i < rows; ++i)
+    {
+        for (size_t j = 0; j < cols; ++j)
+        {
+            arr[i][j] = x++;
+        }
+    }
+}
+
+int main()
+{
+    size_t rows = 5;
+    size_t cols = 5;
+    size_t** array = new size_t * [cols];
+    for (size_t i = 0; i < cols; i++)
+    {
+        array[i] = new size_t[rows];
+    }
+
+    InitializationMatrix(array, cols, rows);
+
+    OutputMatrix(array, cols, rows);
+    size_t position = 3;
+    AddColInMatrixStart(array, cols, rows,position);
+
+    OutputMatrix(array, cols, rows);
+
+    return 0;
+}
+
+
