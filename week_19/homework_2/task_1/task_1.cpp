56c803cae4c40ba804e897ff9bd47d3e72ef7b32
Westilier <vlad2007Gvv@gmail.com>
2024-06-20 23:08:49 +0300

    дз 19.2

---
+++ week_19/homework_2/task_1/task_1.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+
+struct car {
+    std::string color;
+    std::string model;
+    std::string number;
+};
+
+void Initialization(car& car)
+{
+    std::cout << "Введите цвет автомобиля (в И.п) ";
+    getline(std::cin, car.color);
+    std::cout << "Введите модель автомобиля (в И.п) ";
+    getline(std::cin, car.model);
+    std::cout << "Введите номер автомобиля ";
+    getline(std::cin, car.number);
+}
+
+void OutputCar(car const& car)
+{
+    std::cout << "Цвет автомобиля " << car.color << std::endl
+            << "Модель автомобиля " << car.model << std::endl
+            << "Номер автомобиля" << car.number << std::endl;
+}
+
+void EditCar(car& car)
+{
+    std::cout << "Введите новый цвет автомобиля" << std::endl;
+    getline(std::cin, car.color);
+    std::cout << "Введите новую модель автомобиля" << std::endl;
+    getline(std::cin, car.model);
+    std::cout << "Введите новый номер автомобиля" << std::endl;
+    getline(std::cin, car.number);
+}
+
+void OutputAllCar(std::vector<car> const& cars)
+{
+    for (size_t i = 0; i < cars.size(); ++i)
+    {
+        OutputCar(cars[i]);
+    }
+}
+
+int SearchByNumber(std::vector<car> cars, std::string number)
+{
+    for (size_t i = 0; i < cars.size(); ++i)
+    {
+        if (cars[i].number == number)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    return 0;
+}
---
+++ week_19/homework_2/task_2/task_2.cpp
@@ -0,0 +1,142 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <windows.h>
+
+
+union charact
+{
+    bool artiodactyl;
+    int iq;
+    double flightSpeed;
+};
+
+struct LivingEntity
+{
+    std::string type;
+    charact charact;
+    std::string color;
+    double speed;
+};
+
+void Initialization(LivingEntity& entity)
+{
+    std::cout << "Введите тип(птица, скот, человек) ";
+    std::cin >> entity.type;
+    if (entity.type == "птица")
+    {
+        std::cout << "Введите скорость полёта ";
+        std::cin >> entity.charact.flightSpeed;
+    }
+    else if (entity.type == "скот")
+    {
+        std::cout << "Оно парнокопытное? (Y/N) ";
+        char answer;
+        std::cin >> answer;
+        if (answer == 'n' || answer == 'N')
+        {
+            entity.charact.artiodactyl = false;
+        }
+        if (answer == 'y' || answer == 'Y')
+        {
+            entity.charact.artiodactyl = true;
+        }
+    }
+    else if (entity.type == "человек")
+    {
+        std::cout << "Введите IQ ";
+        std::cin >> entity.charact.iq;
+    }
+    std::cout << "Введите скорость передвижения ";
+    std::cin >> entity.speed;
+    std::cout << "Введите цвет ";
+    std::cin >> entity.color;
+}
+
+void OutputEntity(LivingEntity const& entity)
+{
+    if (entity.type == "птица")
+    {
+        std::cout << "Cкорость полёта " << entity.charact.flightSpeed << std::endl;
+    }
+    else if (entity.type == "скот")
+    {
+        std::cout << "Оно парнокопытное (Y/N) ";
+        if (entity.charact.artiodactyl)
+        {
+            std::cout << "Оно парнокопытное" << std::endl;
+        }
+        else
+        {
+            std::cout << "Оно не парнокопытное" << std::endl;;
+        }
+    }
+    else if (entity.type == "человек")
+    {
+        std::cout << "IQ " << entity.charact.iq << std::endl;
+    }
+    std::cout << "Cкорость передвижения " << entity.speed << std::endl;
+    std::cout << "Цвет " << entity.color << std::endl;
+};
+
+void OutputAllEntity(std::vector<LivingEntity> const& entities)
+{
+    for (size_t i = 0; i < size(entities); ++i)
+    {
+        OutputEntity(entities[i]);
+    }
+}
+
+void EditEntity(LivingEntity & entity)
+{
+    std::cout << "Введите новый тип(птица, скот, человек) ";
+    std::cin >> entity.type;
+    if (entity.type == "птица")
+    {
+        std::cout << "Введите новую скорость полёта ";
+        std::cin >> entity.charact.flightSpeed;
+    }
+    else if (entity.type == "скот")
+    {
+        std::cout << "Оно парнокопытное? (Y/N) ";
+        char answer;
+        std::cin >> answer;
+        if (answer == 'n' || answer == 'N')
+        {
+            entity.charact.artiodactyl = false;
+        }
+        if (answer == 'y' || answer == 'Y')
+        {
+            entity.charact.artiodactyl = true;
+        }
+    }
+    else if (entity.type == "человек")
+    {
+        std::cout << "Введите новый IQ ";
+        std::cin >> entity.charact.iq;
+    }
+    std::cout << "Введите новую скорость передвижения ";
+    std::cin >> entity.speed;
+    std::cout << "Введите новый цвет ";
+    std::cin >> entity.color;
+}
+
+int SearchByCharact(std::vector<LivingEntity> entities, charact charact)
+{
+    for (size_t i = 0; i < entities.size(); ++i)
+    {
+        if ((entities[i].charact.iq == charact.iq)
+            ||(entities[i].charact.flightSpeed == charact.flightSpeed)
+            ||(entities[i].charact.artiodactyl == charact.artiodactyl))
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+}
