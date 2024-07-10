04290a44170acdb8af7d52f48298ad540d2ab627
Westilier <vlad2007Gvv@gmail.com>
2024-07-10 20:51:49 +0300

    экзамен не доделан

--- exam/sings/sings/sings.cpp
+++ exam/sings/sings/sings.cpp
@@ -2,39 +2,35 @@
 #include <fstream>
 #include <string>
 #include <windows.h>
+#include <filesystem>
 
-struct Sing
-{
-    std::string name;
-    std::string author;
-    size_t year;
-};
 
-void AddSing(std::string NameFileWithSings)
+bool IsFileByPath(std::string path)
 {
-    std::ofstream Sings(NameFileWithSings,std::ios::app);
+    for (const auto& entry : std::filesystem::directory_iterator("Sings"))
+    {
+        std::filesystem::path FileName = entry.path();
+        if (FileName.string() == path)
+        {
+            std::cout << 10;
+            return true;
+        }
+    }
+    std::cout << 20;
+    return false;
+}
+
+void InputTextInFile(std::string path)
+{
+    std::ofstream Sing(path,std::ios::app);
     std::string buffer;
-
-    std::cout << "Название песни:" << std::endl;
-    getline(std::cin, buffer);
-    Sings <<"Название: " << buffer << "\n";
-
-    std::cout << "Автор песни:" << std::endl;
-    getline(std::cin, buffer);
-    Sings << "Автор: " << buffer << "\n";
-
-    size_t year;
-    std::cout << "Год выпуска песни:" << std::endl;
-    std::cin >> year;   
-    Sings << "Год выпуска: " << year << "\n";
-
     size_t key;
     do
     {
         std::cout << "1 - Ввести текст с клавиатуры" << std::endl
-                << "2 - Ввести текст из файла" << std::endl;
+            << "2 - Ввести текст из файла" << std::endl;
         std::cin >> key;
-        Sings << "Текст ";
+        Sing << "Текст ";
         if (key == 1)
         {
             std::cout << "Для окончания ввода введите 0" << std::endl;
@@ -45,62 +41,129 @@ void AddSing(std::string NameFileWithSings)
                 {
                     break;
                 }
-                Sings << buffer << "\n";
-            } 
+                Sing << buffer << std::endl;
+            }
         }
         else if (key == 2)
         {
-            std::string pathToFile = "voidText.txt";
+            std::string pathToFile;
             std::cout << "Введите путь до файла:";
-            std::cin.ignore(20,'\n');
+            std::cin.ignore(20, '\n');
             getline(std::cin, pathToFile);
             std::cout << pathToFile;
             std::ifstream in(pathToFile);
             while (getline(in, buffer))
             {
-                std::cout << 1;
-                Sings << buffer << "\n";
+                Sing << buffer << std::endl;
             }
             in.close();
         }
     } while (key != 1 && key != 2);
-    Sings.close();
 }
-void DeleteTextSing(std::string NameFileWithSings, std::string NameSing, std::string AuthorSing)
+
+void AddSing(std::string Directory)
 {
     std::string buffer;
-    std::ifstream in(NameFileWithSings);
-    std::ofstream temp("temp.txt");
-    std::string NameSingForSearch = "Название: " + NameSing;
-    std::string AuthorSingForSearch = "Автор: " + AuthorSing;
-    bool condition = false;
-    while (getline(in, buffer))
+    
+    std::cout << "Название песни:" << std::endl;
+    getline(std::cin, buffer);
+    std::string path = Directory + "\\" + buffer + ".txt";
+    size_t i = 1;
+    while (IsFileByPath(path))
     {
-        if (!condition)
+        path = Directory + "\\" + buffer + "(" + std::to_string(i) + ")" + ".txt";
+        i++;
+    }
+    std::ofstream Sing(path);
+
+    std::cout << "Автор песни:" << std::endl;
+    getline(std::cin, buffer);
+    Sing << "Автор: " << buffer << std::endl;
+
+    size_t year;
+    std::cout << "Год выпуска песни:" << std::endl;
+    std::cin >> year;   
+    Sing << "Год выпуска: " << year << std::endl;
+
+    InputTextInFile(path);
+    
+    Sing.close();
+}
+
+void DeleteTextSing(std::string Directory, std::string NameSing, std::string AuthorSing)
+{
+    std::string path = Directory + "\\" + NameSing;
+    std::string AuthorSingForSearch = "Автор: " + AuthorSing;
+
+    for (const auto& entry : std::filesystem::directory_iterator("Sings"))
+    {
+        std::filesystem::path pathToFile = entry.path();
+        std::string pathToFile_str = pathToFile.string();
+        if (pathToFile_str.find(path) != std::string::npos)
         {
-            temp << buffer <<"\n";
-        }
-        else if (condition && buffer.find("Название: "))
-        {
-            condition = false;
-        }
-        if (buffer.find(NameSingForSearch))
-        {
-            getline(in, buffer);
-            temp << buffer << "\n";
-            if (buffer.find(AuthorSingForSearch))
+            std::ifstream Sing(pathToFile_str);
+
+            std::string buffer;
+            getline(Sing, buffer);
+            if (buffer == AuthorSingForSearch)
             {
-                condition = true;
+                Sing.close();
+                remove(pathToFile_str.c_str());
+            }
+
+        }
+    }
+}
+void EditTextInFile(std::string Directory, std::string NameSing, std::string AuthorSing) 
+{
+    std::string tempPath = Directory + "\\" + "temp.txt";
+
+    std::string filePath = Directory + "\\" + NameSing;
+    std::string AuthorSingForSearch = "Автор: " + AuthorSing;
+
+    for (const auto& entry : std::filesystem::directory_iterator("Sings"))
+    {
+        std::filesystem::path pathToFile = entry.path();
+        std::string pathToFile_str = pathToFile.string();
+        if (pathToFile_str.find(filePath) != std::string::npos)
+        {
+            std::ifstream Sing(pathToFile_str);
+            std::ofstream temp(tempPath);
+
+            std::string buffer;
+            getline(Sing, buffer);
+            if (buffer == AuthorSingForSearch)
+            {
+                temp << buffer;
+                while (getline(Sing, buffer))
+                {
+                    temp << buffer;
+                    if (buffer != "Текст")
+                    {
+                        break;
+                    }
+                }
+                InputTextInFile(NameSing);
+                Sing.close();
+                rename(tempPath.c_str(), filePath.c_str());
             }
         }
     }
 }
+    
+
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    std::string NameFileWithSings = "Sings.txt";
-    AddSing(NameFileWithSings);
-    DeleteTextSing(NameFileWithSings, "123", "123");
+    std::string str = "Sings\\123.txt";
+    std::string Directory = "Sings";
+    AddSing(Directory);
+    DeleteTextSing(Directory, "123", "123");
 }
+
+
+
+
+
 
---
+++ exam/tag/tag/tag.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+
+using map = std::vector<std::vector<size_t>>;
+
+void PrintMap(map& map, size_t& size)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        for (size_t j = 0; j < size; ++j)
+        {
+            if (map[i][j] < 10)
+            {
+                std::cout << " " << map[i][j] << " ";
+            }
+            else
+            {
+                std::cout << map[i][j] << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+void CreateMap(map& map, size_t& size)
+{
+    size_t x = 1;
+    for (size_t i = 0; i < size; ++i)
+    {
+        for (size_t j = 0; j < size; ++j)
+        {
+            map[i][j] = x++;
+        }
+    }
+}
+
+
+int main()
+{
+    size_t SIZE=1;
+
+    std::cout << "Выберите режим " << std::endl
+            << "1 - 3 на 3" << std::endl
+            << "2 - 4 на 4" << std::endl;
+    size_t key;
+    std::cin >> key;
+    if (key == 1)
+    {
+        SIZE = 3;
+    }
+    else if (key == 2)
+    {
+        SIZE = 4;
+    }
+    map Map(SIZE, std::vector<size_t>(SIZE));
+    CreateMap(Map, SIZE);
+    PrintMap(Map, SIZE);
+}
